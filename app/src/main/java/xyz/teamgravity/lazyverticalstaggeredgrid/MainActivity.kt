f16e8358a08572ee699c47d54eca923b41ca1016
raheemadamboev <vividricemaryandmax@gmail.com>
2023-02-23 23:26:12 +0500

    Implemented LazyVerticalStaggeredGrid;

--- app/src/main/java/xyz/teamgravity/lazyverticalstaggeredgrid/MainActivity.kt
+++ app/src/main/java/xyz/teamgravity/lazyverticalstaggeredgrid/MainActivity.kt
@@ -3,16 +3,23 @@ package xyz.teamgravity.lazyverticalstaggeredgrid
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.staggeredgrid.LazyVerticalStaggeredGrid
+import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
+import androidx.compose.foundation.lazy.staggeredgrid.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.unit.dp
 import xyz.teamgravity.lazyverticalstaggeredgrid.ui.theme.LazyVerticalStaggeredGridTheme
 
 class MainActivity : ComponentActivity() {
+
+    @OptIn(ExperimentalFoundationApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -21,6 +28,23 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
+                    LazyVerticalStaggeredGrid(
+                        columns = StaggeredGridCells.Adaptive(100.dp),
+                        contentPadding = PaddingValues(16.dp),
+                        horizontalArrangement = Arrangement.spacedBy(16.dp),
+                        verticalArrangement = Arrangement.spacedBy(16.dp),
+                        modifier = Modifier.fillMaxSize()
+                    ) {
+                        items(ListData.value) { item ->
+                            Box(
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .height(item.height)
+                                    .clip(RoundedCornerShape(10.dp))
+                                    .background(item.color)
+                            )
+                        }
+                    }
                 }
             }
         }
