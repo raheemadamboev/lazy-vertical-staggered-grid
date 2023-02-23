abb7a852233cad8267a1a30bece4b1b8954e2b20
raheemadamboev <vividricemaryandmax@gmail.com>
2023-02-23 23:25:26 +0500

    Added List Model;

---
+++ app/src/main/java/xyz/teamgravity/lazyverticalstaggeredgrid/ListModel.kt
@@ -0,0 +1,9 @@
+package xyz.teamgravity.lazyverticalstaggeredgrid
+
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.Dp
+
+data class ListModel(
+    val height: Dp,
+    val color: Color,
+)
